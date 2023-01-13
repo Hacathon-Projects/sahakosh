e0ae7958fa530b8d7f58582fcb799582f7e03592
unknown <aayushgelal@gmail.com>
2023-01-13 10:11:09 +0545

    calendar done

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:sahakosh/calendar_ui/calendarscreen.dart';
 
 import 'package:sahakosh/core/routes/route.dart';
 import 'package:sahakosh/models/investor.dart';
@@ -30,12 +29,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.blue[800],
-          textTheme: GoogleFonts.nunitoTextTheme(),
-        ),
-        home: ScheduleMeetingPage());
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue[800],
+        textTheme: GoogleFonts.nunitoTextTheme(),
+      ),
+      onGenerateRoute: Routes.generateRoute,
+      initialRoute: RouteConstant.landingScreen,
+    );
   }
 }
